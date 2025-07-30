e7c9b00506c4b6bd45bb371d7343c7d32179288a
spranav007 <pranavshakthi007@gmail.com>
2025-07-30 13:35:14 +0530

    changed struc

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import AzureVisionApp from './AzureVisionApp';
+
+export default function App() {
+  return <AzureVisionApp />;
+}
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <h1 className="text-2xl font-bold text-center mt-10">Hello React with Tailwind!</h1>;
+}
